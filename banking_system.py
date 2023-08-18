05d9f9bc552e9c131a97fb182a70603f213b068b
Walter J. Silva <walter4web@gmail.com>
2023-08-18 11:25:13 -0300

    :sparkles: feat: add screen return after withdraw && deposit process

--- banking_system.py
+++ banking_system.py
@@ -25,6 +25,7 @@ while True:
         if valor > 0:
             saldo += valor
             extrato += f"Depósito: R$ {valor:.2f}\n"
+            print("\nDepósito realizado com sucesso!\n")
         
         else:
             print("Operação falhou! Valor informado inválido")
@@ -33,33 +34,32 @@ while True:
         valor = float(input("Informe o valor do saque: "))
 
         excedeu_saldo = valor > saldo
-
         excedeu_limite = valor > limite
-
         excedeu_saques = numero_saques >= LIMITE_SAQUES
 
         if excedeu_saldo:
-            print("Falha na operação! Você não tem saldo suficiente.")
+            print("Falha na operação! Você não tem saldo suficiente.\n")
 
         elif excedeu_limite:
-            print("Falha na operação! O valor do saque excede o limite de saque único.")
+            print("Falha na operação! O valor do saque excede o limite de saque único.\n")
 
         elif excedeu_saques:
-            print("Falha na operação! Número máximo de saques excedido.")
+            print("Falha na operação! Número máximo de saques excedido.\n")
         
         elif valor > 0:
             saldo -= valor
             extrato += f"Saque: R$ {valor:.2f}\n"
             numero_saques += 1
+            print("\nSaque realizado. Recolha seu dinheiro.\n")
 
         else:
-            print("Falha na operação! O valor informado é inválido.")
+            print("Falha na operação! O valor informado é inválido.\n")
 
-    elif  opcao == "e":
-        print("\n=============== EXTRATO ===============")
+    elif opcao == "e":
+        print("\n=============== EXTRATO ===============\n")
         print("\nNão foram realizadas movimentações." if not extrato else extrato)
         print(f"\nSaldo: R$ {saldo:.2f}")
-        print("\n=======================================")
+        print("\n=======================================\n")
 
     elif opcao == "x":
         break
